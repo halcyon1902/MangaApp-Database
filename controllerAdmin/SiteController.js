0250dc578ff33ff8f156c1908abd5d071376c4cd
huutien <huutien3946@gmail.com>
2022-11-23 11:09:41 +0700

    Load duoc truyen tu database

--- controllerAdmin/SiteController.js
+++ controllerAdmin/SiteController.js
@@ -1,7 +1,18 @@
+const { TaiKhoan, Truyen } = require("../model/model");
+const { mutipleMongooseToObject} = require('../util/mongoose')
+
 class SiteController {
   //[Get] /home
-  home(req, res) {
-    res.render("home");
+  home(req, res, next) {
+    Truyen.find({})
+      .then(truyens => {
+        
+        res.render('home',{
+          truyens: mutipleMongooseToObject(truyens)
+        })
+      })
+      .catch(next);
+    
   }
 
   // show thong tin 1 object
